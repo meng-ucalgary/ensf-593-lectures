c1f08fa438118eb0cf84aa2a8478483884ee43eb
Yves <12809436+ypauchard@users.noreply.github.com>
2021-05-31 09:50:21 -0600

    l04 part A in class work.

--- l07/src/StringComparison.java
+++ l07/src/StringComparison.java
@@ -8,9 +8,17 @@ public class StringComparison {
 	 * TODO add all other variable definitions below
 	 * 
 	 * @startuml
-	 * object "''java'', s1" as s1
+	 * object "''java'', s1, s2, s6" as s1
 	 * s1 : value="java"
 	 * 
+	 * object "''Java'', s3" as s3
+	 * s3 : value="Java"
+	 * 
+	 * object s4
+	 * s4 : value="java"
+	 * 
+	 * object s5
+	 * s5 : value="java"
 	 * 
 	 * @enduml
 	 */
@@ -18,16 +26,18 @@ public class StringComparison {
 		String s1 = "java", s2 = "java", s3 = "Java"; 
 		String s4 = new String(s2);
 		String s5 = new String("java");
+		String s6 = "java";
 		
-		System.out.println("a. s1 == s2 ");
-		System.out.println("b. s1.equals(s2) ");
-		System.out.println("c. s1 == s3 ");
-		System.out.println("d. s1.equals(s3) "); 
-		System.out.println("e. s2 == s3 ");
-		System.out.println("f. s2.equals(s4) ");
-		System.out.println("g. s2 == s4 ");
-		System.out.println("h. s1 == s5 "); 
-		System.out.println("i. s4 == s5 ");
+		System.out.println("a. s1 == s2 "+ (s1 == s2));//true
+		System.out.println("b. s1.equals(s2) "+s1.equals(s2));//true
+		System.out.println("c. s1 == s3 "+ (s1 == s3)); //false
+		System.out.println("d. s1.equals(s3) "+s1.equals(s3)); //false
+		System.out.println("e. s2 == s3 "+ (s2 == s3));//false
+		System.out.println("f. s2.equals(s4) "+s2.equals(s4));//true
+		System.out.println("g. s2 == s4 "+ (s2 == s4));//false
+		System.out.println("h. s1 == s5 "+ (s1 == s5)); //false
+		System.out.println("i. s4 == s5 "+ (s4 == s5));//false
+		System.out.println("j. s6 == s1 "+ (s6 == s1));//true
 
 	}
 
--- l07/src/StringManipulation.java
+++ l07/src/StringManipulation.java
@@ -12,8 +12,17 @@ public class StringManipulation {
 		String s2 = "hello world";
 		
 		//TODO a. Swap the front and back half of s1 giving a new string.
+		StringBuilder b1 = new StringBuilder();
+		b1.append(s1.substring(s1.length()/2));
+		b1.append(s1.substring(0, s1.length()/2));
+		System.out.println(b1);
 		
 		//TODO b. Swap ”world” and ”hello” in s2 giving a new string.
+		StringBuffer b2 = new StringBuffer();
+		b2.append(s2.substring(s2.indexOf('w')));
+		b2.append(" ");
+		b2.append(s2.substring(0, s2.indexOf(' ')));
+		System.out.println(b2);
 		
 		//TODO c. Combine parts of s1 and s2 to create a new string ”hello abc”.
 		
