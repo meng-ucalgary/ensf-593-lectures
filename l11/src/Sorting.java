6592f728419372305df635b3d5ebc25960dc8f9e
Yves <12809436+ypauchard@users.noreply.github.com>
2021-06-14 09:47:53 -0600

    l11 part A in class work.

--- l11/src/Sorting.java
+++ l11/src/Sorting.java
@@ -8,13 +8,25 @@ public class Sorting {
 	 */
 	public static void insertionSort(int[] arr, boolean verbose) {
 		
-		//TODO implement insertionSort
+		int x;
 		
-
-//			if(verbose) {
-//				System.out.print("k="+k+": ");
-//				System.out.println(Arrays.toString(arr));
-//			}
+		//TODO implement insertionSort
+		for(int k = 1; k<arr.length; k++) {
+			
+			x = arr[k];
+			int i;
+			for(i = k-1; i>=0 && arr[i]> x; i--) {
+				arr[i+1] = arr[i];
+			}
+			
+			arr[i+1] = x;
+			
+			
+			if(verbose) {
+				System.out.print("k="+k+": ");
+				System.out.println(Arrays.toString(arr));
+			}
+		}
 		
 	}
 
@@ -25,12 +37,24 @@ public class Sorting {
 	public static void insertionSort(Comparable[] arr, boolean verbose) {
 		
 		//TODO implement insertionSort
+		Comparable x;
 		
-
-//			if(verbose) {
-//				System.out.print("k="+k+": ");
-//				System.out.println(Arrays.toString(arr));
-//			}
+		for(int k = 1; k<arr.length; k++) {
+			
+			x = arr[k];
+			int i;
+			for(i = k-1; i>=0 && arr[i].compareTo(x) > 0; i--) {
+				arr[i+1] = arr[i];
+			}
+			
+			arr[i+1] = x;
+			
+			
+			if(verbose) {
+				System.out.print("k="+k+": ");
+				System.out.println(Arrays.toString(arr));
+			}
+		}
 		
 	}
 
@@ -41,12 +65,27 @@ public class Sorting {
 	public static void selectionSort(int[] arr, boolean verbose) {
 		
 		//TODO implement selectionSort
-		
-//			if(verbose) {
-//				System.out.print("k="+k+": ");
-//				System.out.println(Arrays.toString(arr));
-//			}
-		
+		for(int k=0; k<arr.length-1; k++) {
+			int smallest = k;
+			//find smallest to the right
+			for(int i = k+1; i<arr.length; i++) {
+				if(arr[i]<arr[smallest]) {
+					smallest=i;
+				}
+			}
+			
+			//swap
+			if(k!=smallest) {
+				int temp = arr[k];
+				arr[k] = arr[smallest];
+				arr[smallest] = temp;
+			}
+			
+			if(verbose) {
+				System.out.print("k="+k+": ");
+				System.out.println(Arrays.toString(arr));
+			}
+		}
 	}
 
 	public static void main(String[] args) {
@@ -75,17 +114,17 @@ public class Sorting {
 		System.out.println("Sorted with insertion sort: ");
 		insertionSort(c, true);
 		System.out.println(Arrays.toString(c));
-
-		// Testing Comparable insertionSort() with LetterFreq
-		LetterFreq[] letters = {new LetterFreq('A', 10), 
-				new LetterFreq('B', 5),
-				new LetterFreq('C', 8)};
-
-		System.out.println("Original LetterFreq array: ");
-		System.out.println(Arrays.toString(letters));
-		System.out.println("Sorted with insertion sort: ");
-		insertionSort(letters, true);
-		System.out.println(Arrays.toString(letters));
+//
+//		// Testing Comparable insertionSort() with LetterFreq
+//		LetterFreq[] letters = {new LetterFreq('A', 10), 
+//				new LetterFreq('B', 5),
+//				new LetterFreq('C', 8)};
+//
+//		System.out.println("Original LetterFreq array: ");
+//		System.out.println(Arrays.toString(letters));
+//		System.out.println("Sorted with insertion sort: ");
+//		insertionSort(letters, true);
+//		System.out.println(Arrays.toString(letters));
 
 	}
 
