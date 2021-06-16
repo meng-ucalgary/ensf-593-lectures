5a18ecd4ba418304d10e54e30ed7926f89c4cca2
Yves <12809436+ypauchard@users.noreply.github.com>
2021-06-16 08:43:12 -0600

    l12 add main for video implementation.

---
+++ l12/src/video/ExpressionTester.java
@@ -0,0 +1,33 @@
+package video;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExpressionTester {
+
+	public static void main(String[] args) {
+		Expr e1 = new Times(new Const(3.0),
+							new Plus(new Var("y"),
+									new Var("x")));
+		
+		Expr e2 = new Plus(new Times(new Const(3.0),
+									 new Var("y")),
+						   new Var("x"));
+		
+		System.out.println(e1);
+		System.out.println(e2);
+		
+		
+		Map<String, Double> env = new HashMap<String, Double>();
+		
+		env.put("x", 2.0);
+		env.put("y", 4.0);
+		
+		//18
+		System.out.println("Expression 1" + e1 + " with x=2, y=4 -> "+e1.eval(env));
+		//14
+		System.out.println("Expression 2" + e2 + " with x=2, y=4 -> "+e2.eval(env));
+
+	}
+
+}
